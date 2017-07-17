66c755f1bdb4fc3b9a19224ae4636f64168ae801
csongorur <urcsongor@gmail.com>
2017-07-17 15:23:31 +0300

    git init

---
+++ RegexTool/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Gtk;
+using System.Text.RegularExpressions;
+
+public partial class MainWindow: Gtk.Window
+{
+	private String backRegex;
+	private String backReplace;
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnBtnOkClicked (object sender, EventArgs e)
+	{
+		String result;
+		String text;
+		backRegex = txtRegex.Text;
+		backReplace = txtReplace.Text;
+		text = txtText.Buffer.Text;
+		Regex regex = new Regex (backRegex);
+		result = regex.Replace (text,backReplace);
+		display.Buffer.Text = result;
+	}
+
+	protected void OnBtnBackClicked (object sender, EventArgs e)
+	{
+		txtRegex.Text = "";
+		txtReplace.Text = "";
+		txtText.Buffer.Text = "";
+		display.Buffer.Text = "";
+	}
+}
